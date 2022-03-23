d65dc34911cc753bfe48c9bb3f8d78f387a11f98
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-03-24 08:44:08 +0900

    タスク一覧まで作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController; // 追加するのを忘れない
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [TasksController::class, 'index'])->name('tasks.index');
 
 Route::get('/about', function () {
     return view('about');
