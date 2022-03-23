610b8e57b61a0494dd6394cca95f86202736cacc
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-03-23 22:45:03 +0900

    tailwind導入

--- src/resources/views/about.blade.php
+++ src/resources/views/about.blade.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
+    <link href="/css/app.css" rel="stylesheet">
 </head>
 
 <body>
-    <h1 class="text-3xl">About</h1>
+    <h1 class="text-xs">About</h1>
 </body>
 
 </html>
